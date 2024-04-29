6fd43b9e716ff8a0bafadd61bb4df162f5cc41f7
FerMora <f.m199698@gmail.com>
2024-04-28 21:13:22 -0600

    Generando el menu seleccionable

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,16 @@
+import java.awt.*;
+import java.io.IOException;
+
 /*
 Proyecto de JAVA con el cual se podra realizar conversion de dolares a otras
 monedas, por medio de un API
  */
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hola");
+    public static void main(String[] args) throws IOException, InterruptedException {
+        Menu_principal menuPrincipal = new Menu_principal();
+        menuPrincipal.SaludoPrincipal();
+
+        menuPrincipal.MenuP();
 
     }
 }
